fc549e0f0bde2ccf79d556e7a3ba41f389632714
RAPHAEL DE LEMOS PIRES <raphaellpires@gmail.com>
2026-04-09 08:52:41 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
+        Department departmentBooks = new Department(1, "Books");
 
-        IO.println(department);
+        IO.println(departmentBooks);
+
+        Seller seller1 = new Seller(1, "João", "joao@gmail.com", new Date() ,3500.00, departmentBooks);
+
+        System.out.println(seller1);
 
     }
 
